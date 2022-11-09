606f092bb16a25c6ef0fe666a3467e985b316684
cqd <amos.cao@outlook.com>
2022-11-10 00:20:20 +0800

    initalize

---
+++ Model/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BlazingPizza
+{
+    /// <summary>
+    /// Represents a customized pizza as part of an order
+    /// </summary>
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+    
+        public string Name { get; set; }
+    
+        public string Description { get; set; }
+    
+        public decimal Price { get; set; }
+    
+        public bool Vegetarian { get; set; }
+    
+        public bool Vegan { get; set; }
+    }
+}
---
+++ Service/PizzaService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazingPizza.Data;
+
+public class PizzaService
+{
+    public async Task<Pizza[]> GetPizzasAsync()
+    {
+        return new Pizza[]
+        {
+            new Pizza()
+            {
+                Name = "The Baconatorizor",
+                Description = "It has EVERY kind of bacon",
+                Price = 15,
+                Vegetarian = true,
+                Vegan = true,
+            }
+        };
+    }
+}
+
